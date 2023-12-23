953e129fa90aa71c41b3af96b6428e8c9257f143
timme <timmeromberg@gmail.com>
2023-12-23 15:24:45 +0100

    nginx debug

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
+echo "Starting entrypoint script..."
+
 # Replace environment variables in nginx.conf.template
+echo "Running envsubst..."
 envsubst '$NGINX_HOST' < /etc/nginx/templates/nginx.conf.template > /etc/nginx/nginx.conf
 
+echo "envsubst completed. Starting Nginx..."
 # Start Nginx in the foreground
 exec nginx -g 'daemon off;'
