c82538aba08f5357a28b8a47b4efbdf76ab30220
timme <timmeromberg@gmail.com>
2023-12-23 15:42:40 +0100

    docker

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,8 @@
 #!/bin/sh
 
 echo "Starting entrypoint script..."
-
 # Replace environment variables in nginx.conf.template
-echo "Running envsubst..."
 envsubst '$CERTBOT_DOMAIN' < /etc/nginx/templates/nginx.conf.template > /etc/nginx/nginx.conf
 
-echo "envsubst completed. Starting Nginx..."
-# Start Nginx in the foreground
-exec nginx -g 'daemon off;'
+echo "envsubst completed. Ready for starting Nginx..."
+# Don't start Nginx here; just prepare the configuration.
