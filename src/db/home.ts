dc6b3fda35560f130bbff742072b78d693a05a8a
timme <timmeromberg@gmail.com>
2024-10-07 15:18:40 +0200

    pricing updated

--- src/db/home.ts
+++ src/db/home.ts
@@ -1,53 +1,55 @@
 export const sampleImages = [
-  { alt: 'Image here', src: '/images/home/sample-7.png' },
-  { alt: 'Image here', src: '/images/home/sample-8.png' },
-  { alt: 'Image here', src: '/images/home/sample-9.png' },
-  { alt: 'Image here', src: '/images/home/sample-10.png' },
+  { alt: "Image here", src: "/images/home/sample-7.png" },
+  { alt: "Image here", src: "/images/home/sample-8.png" },
+  { alt: "Image here", src: "/images/home/sample-9.png" },
+  { alt: "Image here", src: "/images/home/sample-10.png" },
 ];
 
 export const whyBlueEngineData = [
   {
-    title: 'Verbeterde prestatienormen',
-    description: 'Ingericht om jouw wekelijkse scores zo gezond mogelijk te houden.',
-  },
-  {
-    title: 'Controleert recht op compensatie',
+    title: "Verbeterde prestatienormen",
     description:
-      'Loop niet meer onnodige vergoedingen vanuit Bol.com mis en houd bij wat wel en niet vergoedt is.',
+      "Ingericht om jouw wekelijkse scores zo gezond mogelijk te houden.",
   },
   {
-    title: 'Waardevolle inzichten',
-    description: 'Zie het gehele uitgaande verkeer van jouw bedrijf in een handomdraai.',
+    title: "Controleert recht op compensatie",
+    description:
+      "Loop niet meer onnodige vergoedingen vanuit Bol.com mis en houd bij wat wel en niet vergoedt is.",
+  },
+  {
+    title: "Waardevolle inzichten",
+    description:
+      "Zie het gehele uitgaande verkeer van jouw bedrijf in een handomdraai.",
   },
 ];
 
 export const transparentPricingData = [
   {
-    title: 'Starter',
-    price: '€ 19,95',
-    subscriptionDetail: 'Ex. BTW',
-    subscriptionTime: 'maand',
-    features: ['Tot 100 orders p.m.'],
+    title: "Starter",
+    price: "€ 19,95",
+    subscriptionDetail: "Ex. BTW",
+    subscriptionTime: "maand",
+    features: ["Tot 100 orders p.m."],
   },
   {
-    title: 'Groei',
-    price: '€ 34,95',
-    subscriptionDetail: 'Ex. BTW',
-    subscriptionTime: 'maand',
-    features: ['Tot 250 orders p.m.'],
+    title: "Groei",
+    price: "€ 29,95",
+    subscriptionDetail: "Ex. BTW",
+    subscriptionTime: "maand",
+    features: ["Tot 250 orders p.m."],
   },
   {
-    title: 'Business',
-    price: '€ 49,95',
-    subscriptionDetail: 'Ex. BTW',
-    subscriptionTime: 'maand',
-    features: ['Tot 500 orders p.m'],
+    title: "Business",
+    price: "€ 49,95",
+    subscriptionDetail: "Ex. BTW",
+    subscriptionTime: "maand",
+    features: ["Tot 500 orders p.m"],
   },
   {
-    title: 'Premium',
-    price: '€ 69,95',
-    subscriptionDetail: 'Ex. BTW',
-    subscriptionTime: 'maand',
-    features: ['Tot 1000 orders p.m.'],
+    title: "Premium",
+    price: "€ 69,95",
+    subscriptionDetail: "Ex. BTW",
+    subscriptionTime: "maand",
+    features: ["Tot 1000 orders p.m."],
   },
 ];
